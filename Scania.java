c90ba9f9e269e5f286b346c315b6eacaecabdc09
samibr96 <117283229+samibr96@users.noreply.github.com>
2023-01-07 18:58:02 +0100

    Update Scania.java
    initialized angel in the constructor as a name Liftbed.
    
    Change in function Platform from angle between 0 and 70, to angle work with true and false.

--- Scania.java
+++ Scania.java
@@ -1,88 +1,34 @@
 
 import java.awt.*;
 
-public class Scania extends Truck { //// change to implment and ask them
+public class Scania extends Truck {
 
-    private double angle; /// Ask him if it's true
-
-    private double platformAngle = 0;
-
+ 
+    private boolean platformAngle = true; // It mean the angle if true, so the angle is 0. the angle is false, it mean 70
     private double trimFactor = 1.25;
 
-
-    private Ramp ramp;
-
     public Scania(Direction direction , double x, double y) {
-        super("Scania", 2, 4, 0, Color.RED, Direction.SOUTH , x, y);
+        super("Scania", 2, 4, 0, Color.RED, Direction.SOUTH , x, y, true);
 
     }
 
     public double speedFactor() {return getEnginePower() * 0.01 * trimFactor;}
 
     
-    public boolean raisePlatform(){ //// true
-        return ramp.raisePlatform();
-    }
 
-    public void lowerPlatform(int amount){
-        if(getCurrentSpeed() == 0 && amount <= 70 && amount >= 0) {
+    public void adjPlatform(boolean amount){
+        if(getCurrentSpeed() == 0 && amount == false) {
             platformAngle =  amount;
         }  
     }
 
-    public double getPlatformAngle(){
+    public boolean getPlatformAngle(){
         return platformAngle;
     }
 
     protected boolean Move(){
-        return getPlatformAngle() == 0;
+        return getPlatformAngle() == true;
     }
-
-    
-    
-    
-    
-    
-    
-    public void increaseAngle(double amount) 
-    
-    {
-
-        double res = angle + amount;
-
-        if (getCurrentSpeed() == 0) {
-            if ( res > 70) 
-            {
-                angle = 70;
-            } 
-            else
-            angle = angle + amount;
-        }
-    }  //override gas
-        // start engien
-
-    public void decreaseAngle(double amount) 
-    {
-        double res = angle - Math.abs(amount);
-
-        if ( res < 0) 
-        {
-            angle = 0;
-        } 
-        else
-        angle = angle - Math.abs(amount);
-    }
-
     
 
-
-    
-
-
-   
-
-
-    
-    
-    
 }
